d014836497a67b4522cf36e34a19ef91fe4ee056
Erin Kennedy <erin@robotgrrl.com>
2024-06-30 09:39:10 -0400

    More reliable stop and restart of Task before watchdog

--- firmware/RobotButterflyTempHumidMulti/RobotButterflyTempHumidMulti.ino
+++ firmware/RobotButterflyTempHumidMulti/RobotButterflyTempHumidMulti.ino
@@ -2,11 +2,14 @@
  * Robot Butterfly Temperature & Humidity Multitask
  * -------------------------------------------------
  * Sample the DHT11 sensor in a separate task, transfer
- * the data via a queue. Restart the task if needed.
+ * the data via a queue. Timer to stop and restart the 
+ * task if data not received before the core 1 watchdog 
+ * restarts the ESP.
  * 
  * Board: ESP32 Dev Module
  * CPU speed: 80 MHz
  * Upload speed: 115200 baud
+ * esp boards package: v 3.0.2
  *
  * 27 Jun 2024 - Erin RobotZwrrl for Robot Missions Inc
  * http://robotzwrrl.xyz/robot-butterfly
@@ -32,27 +35,25 @@ volatile bool updateTH = false;
 volatile bool restartTH = false;
 volatile uint32_t restartTH_count = 0;
 uint32_t valuesTH_count = 0;
+long last_print;
 
 struct RawDataPoint_TH {
   int temp_raw;
   int humid_raw;
   long record_time;
 };
-struct RawDataPoint_TH raw_data_th;
+struct RawDataPoint_TH raw_th;
 
 void IRAM_ATTR Timer0_ISR() {
   updateTH = true;
 
   // check that the sensor is still sending data recently
-  if(millis()-raw_data_th.record_time > 1500 && millis() > STARTUP_WAIT_TH && restartTH == false) {//SAMPLE_FREQ_TH) {
+  if(millis()-raw_th.record_time > SAMPLE_FREQ_TH && millis() > STARTUP_WAIT_TH && restartTH == false) {
     restartTH = true;
     restartTH_count++;
   }
 
 }
-
-
-long last_print;
 
 
 void setup() {
@@ -65,12 +66,12 @@ void setup() {
 
   // temphumid data digester
   // set up timer 0 for every 0.5 second
-  // params: timer #0, prescaler amount, count up (true)
-  timer0_cfg = timerBegin(0, 16000, true);
-  timerAttachInterrupt(timer0_cfg, &Timer0_ISR, true);
-  // params: timer, tick count, auto-reload (true)
-  timerAlarmWrite(timer0_cfg, 2500, true);
-  timerAlarmEnable(timer0_cfg);
+  timer0_cfg = timerBegin(16000);
+  timerAttachInterrupt(timer0_cfg, &Timer0_ISR);
+  timerWrite(timer0_cfg, 2500);
+  // params: timer, tick count, auto-reload (true), reload count (0 = infinite)
+  timerAlarm(timer0_cfg, 0, true, 0);
+  timerStart(timer0_cfg);
 
   initTempHumid();
 
@@ -114,7 +115,7 @@ void Task_TH_code(void * pvParameters) {
 
     if(DEBUG_TH) Serial << "Reading DHT11" << endl;
 
-    if(millis() < STARTUP_WAIT_TH) continue; // doesn't work on startup
+    if(millis() < STARTUP_WAIT_TH) continue; // let sensor warm up
 
     if(dht11.readTemperatureHumidity(temp_raw, humid_raw) != 0) {
       Serial << millis() << " [" << xPortGetCoreID() << "] ";
@@ -160,6 +161,14 @@ void loop() {
 
   updateTempHumid();
 
+  // for testing purposes
+  if(Serial.available()) {
+    char c = Serial.read();
+    if(c == 'r') {
+      restartTH = true;
+    }
+  }
+
 }
 
 
@@ -167,26 +176,25 @@ void updateTempHumid() {
 
   if(restartTH) {
     Serial << "Restarting Task_TH " << restartTH_count << endl;
-    vTaskSuspend(Task_TH);
+    xTaskAbortDelay(Task_TH);
     vTaskDelete(Task_TH);
-    delay(50); // necessary delay it seems, 10 ms is too short
     initTempHumid();
     restartTH = false;
   }
 
   if(!updateTH) return;
 
-  uint8_t messages_ready_count = 0;
+  uint8_t ready = 0;
   int a;
   int b;
   long c;
 
   // check the queues
-  if(uxQueueMessagesWaiting(Queue_TH1) > 0) messages_ready_count++;
-  if(uxQueueMessagesWaiting(Queue_TH2) > 0) messages_ready_count++;
-  if(uxQueueMessagesWaiting(Queue_TH3) > 0) messages_ready_count++;
+  if(uxQueueMessagesWaiting(Queue_TH1) > 0) ready++;
+  if(uxQueueMessagesWaiting(Queue_TH2) > 0) ready++;
+  if(uxQueueMessagesWaiting(Queue_TH3) > 0) ready++;
   
-  if(messages_ready_count < 3) {
+  if(ready < 3) {
     //Serial << "Queue messages not ready" << endl;
     return;
   }
@@ -196,12 +204,12 @@ void updateTempHumid() {
   xQueueReceive(Queue_TH2, &b, 0);
   xQueueReceive(Queue_TH3, &c, 0);
 
-  raw_data_th.temp_raw = a;
-  raw_data_th.humid_raw = b;
-  raw_data_th.record_time = c;
+  raw_th.temp_raw = a;
+  raw_th.humid_raw = b;
+  raw_th.record_time = c;
   valuesTH_count++;
 
-  Serial << millis() << " Received from queue (" << valuesTH_count << "): " << raw_data_th.temp_raw << ", " << raw_data_th.humid_raw << ", " << raw_data_th.record_time << endl;
+  Serial << millis() << " Received from queue (" << valuesTH_count << "): " << raw_th.temp_raw << ", " << raw_th.humid_raw << ", " << raw_th.record_time << endl;
   
   // flush the queues
   xQueueReset(Queue_TH1);
